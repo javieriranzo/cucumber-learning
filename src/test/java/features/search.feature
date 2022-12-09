d7a2bbae2ec371e75d7af3bcf1bdf35b794dedd7
javieriranzo <javieriranzo92@gmail.com>
2022-12-09 18:35:33 +0100

    Restart Cucumber learning course

--- src/test/java/features/search.feature
+++ src/test/java/features/search.feature
@@ -4,6 +4,6 @@ Feature: Search
   @tag1
   Scenario: Search dresses 
     Given the user enters in the index page
-    When the user enters the name of dress in the search bar
-    And the user clicks search button
-    Then the user goes to dresses page 
+    When the user enters his or her data in the contact form
+    And the user submit  button
+    Then the user is registred
--- src/test/java/steps/SearchSteps.java
+++ src/test/java/steps/SearchSteps.java
@@ -3,27 +3,37 @@ package steps;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SearchSteps {
 
+	WebDriver webDriver;
+
 	@Given("the user enters in the index page")
 	public void userEntersIndexPage() {
-		System.out.println("Hewllo world!");
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+		webDriver = new ChromeDriver();
+		webDriver.navigate().to("https://testingqarvn.com.es/datos-personales/");
 	}
 
-	@When("the user enters the name of dress in the search bar")
-	public void userEntersNameSearchBar() {
-
+	@When("the user enters his or her data in the contact form")
+	public void userEntersPersonalDataInContactForm() {
+		webDriver.findElement(By.id("wsf-1-field-21")).sendKeys("Javier");
+		webDriver.findElement(By.id("wsf-1-field-22")).sendKeys("Iranzo Rebenaque");
+		webDriver.findElement(By.id("wsf-1-field-23")).sendKeys("javieriranzo92@gmail.com");
+		webDriver.findElement(By.id("wsf-1-field-24")).sendKeys("+34666999333");
+		webDriver.findElement(By.id("wsf-1-field-28")).sendKeys("Evergreen Terrace, 42, Springfield");
 	}
 
-	@When("the user clicks search button")
-	public void userClicksSearchButton() {
-
+	@When("the user submit  button")
+	public void userClicksSubmitButton() {
+		webDriver.findElement(By.id("wsf-1-field-27")).click();
 	}
 
-	@Then("the user goes to dresses page")
-	public void userGoesToDressesPage() {
-
+	@Then("the user is registred")
+	public void userIsRegistred() {
 	}
 
 }
