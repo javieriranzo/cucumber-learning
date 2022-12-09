7b1d39a85e879ab32af2bd92401cacbfbd861c5d
javieriranzo <javieriranzo92@gmail.com>
2022-12-09 17:11:04 +0100

    Testing new access token

--- src/test/java/steps/SearchSteps.java
+++ src/test/java/steps/SearchSteps.java
@@ -8,7 +8,7 @@ public class SearchSteps {
 
 	@Given("the user enters in the index page")
 	public void userEntersIndexPage() {
-
+		System.out.println("Hewllo world!");
 	}
 
 	@When("the user enters the name of dress in the search bar")
